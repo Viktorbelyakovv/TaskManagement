d30fff2bf42969443a8ade304ad72da039e60983
Viktorbelyakovv <belyakoviktor97@yandex.ru>
2021-12-14 14:53:43 +0400

    test2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactрлорлордрдр
+          Learn fghnbtgn
+
         </a>
       </header>
     </div>
