429ba1ece082707838ea6502a90f76cea501f82b
Viktorbelyakovv <belyakoviktor97@yandex.ru>
2021-12-23 14:58:34 +0400

    tab-styles

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -5,19 +5,25 @@ import "./Navigation.css";
 
 const Navigation = () => {
   return (
-    <AppBar position="static" className="AppBar">
-      <Tabs>
+    <AppBar
+      position="static"
+      className="AppBar"
+      sx={{ bgcolor: "rgb(70, 70, 70)" }}
+    >
+      <Tabs className="Tab">
         <Tab
           component={NavLink}
           activeClassName="active"
           to="/"
           label="Main Page"
+          sx={{ color: "rgb(250, 235, 96)" }}
         ></Tab>
         <Tab
           component={NavLink}
           activeClassName="active"
           to="/CompletedTaskPage"
           label="Completed Tasks"
+          sx={{ color: "rgb(250, 235, 96)" }}
         ></Tab>
       </Tabs>
     </AppBar>
