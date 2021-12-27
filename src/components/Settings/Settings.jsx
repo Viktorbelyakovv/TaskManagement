ad0b831787d26e22294ec2f1af6496721914f075
Viktorbelyakovv <belyakoviktor97@yandex.ru>
2021-12-27 13:29:48 +0400

    settings-component

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import "./Settings.css";
 
 const Settings = () => {
-  return <div className="se">{"Settings component"}</div>;
+  return (
+    <div>
+      <div>{"Default category"}</div>
+      <div>{"Create new category"}</div>
+      <div>{"List of categories"}</div>
+    </div>
+  );
 };
 
 export default Settings;
